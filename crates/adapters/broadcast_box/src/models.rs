e8e267146a001275377666a0f69365fd49ad1376
richardtatum <rtatum@pm.me>
2026-04-03 20:58:58 +0100

    Parse datetime objects and add viewer struct

--- crates/adapters/broadcast_box/src/models.rs
+++ crates/adapters/broadcast_box/src/models.rs
@@ -1,14 +1,18 @@
+use chrono::{DateTime, Utc};
 use serde::Deserialize;
 
 #[derive(Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct VideoTrack {
     pub bitrate: i32,
-    pub last_keyframe: String, // Datetime
+    pub last_keyframe: DateTime<Utc>,
 }
 
 #[derive(Deserialize, Debug)]
 pub struct AudioTrack {}
+
+#[derive(Deserialize, Debug)]
+pub struct Session {}
 
 #[derive(Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
@@ -16,6 +20,7 @@ pub struct StreamSummary {
     pub stream_key: String,
     pub is_public: bool,
     pub motd: String,
-    pub stream_start: String, // Datetime
+    pub stream_start: DateTime<Utc>,
     pub video_tracks: Vec<VideoTrack>,
+    pub sessions: Vec<Session>,
 }
