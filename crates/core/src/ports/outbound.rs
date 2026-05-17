c58a1c7585808c9b083ede23d15d5c2c1d173cab
Richard Tatum <rtatum@pm.me>
2026-05-17 14:03:34 +0100

    Add new methods to the outbound port

--- crates/core/src/ports/outbound.rs
+++ crates/core/src/ports/outbound.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 
 use crate::models::error::CoreError;
-use crate::models::persistence::{Host, Subscription};
+use crate::models::persistence::Host;
 use crate::models::status::StreamStatus;
 use async_trait::async_trait;
 
@@ -18,7 +18,24 @@ pub trait StreamStatusProvider: Send + Sync {
 // These traits will need result types
 #[async_trait]
 pub trait PersistenceProvider: Send + Sync {
-    async fn get_hosts(&self) -> Result<Vec<Host>, CoreError>;
+    async fn get_hosts(&self, guild_id: i64) -> Result<Vec<Host>, CoreError>;
+
+    async fn add_subscription(
+        &self,
+        host_id: i64,
+        key: String,
+        channel_id: i64,
+        created_by: i64,
+    ) -> Result<i64, CoreError>;
+
+    async fn add_stream(
+        &self,
+        subscription_id: i64,
+        status: StreamStatus,
+        viewer_count: i64,
+        message_id: i64,
+        start_time: String,
+    ) -> Result<i64, CoreError>;
 
     // async fn get_subscriptions(&self, host_url: String) -> Vec<Subscription>;
 }
