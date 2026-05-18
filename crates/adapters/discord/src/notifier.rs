7571d72e3734dcef58493c95ea9201568c7cb64d
Richard Tatum <rtatum@pm.me>
2026-05-18 22:14:25 +0100

    Move the stream link to the header instead of a specific field in the embed

--- crates/adapters/discord/src/notifier.rs
+++ crates/adapters/discord/src/notifier.rs
@@ -145,14 +145,14 @@ impl DiscordNotifier {
         let minutes = duration.num_minutes() % 60;
         let duration_str = format!("{:02}:{:02}", hours, minutes);
         let viewer_str = info.viewers.to_string();
-        let link = format!("{}/{}", self.host.url, self.key);
+        let url = format!("{}/{}", self.host.url, self.key);
 
         let mut embed = CreateEmbed::new()
             .title("Stream Online")
+            .url(url)
             .color(0x2ECC71)
             .description(format!("{} is streaming!", self.key))
             .field(EMPTY_STR, EMPTY_STR, false) // Add a blank line to separate the fields from the description
-            .field("Link", link, false)
             .field("Duration", duration_str, true)
             .field("Viewers", viewer_str, true)
             .footer(CreateEmbedFooter::new(format!(
@@ -169,13 +169,13 @@ impl DiscordNotifier {
 
     fn offline_embed(&self, playing: Option<&str>) -> CreateEmbed {
         let ended = Utc::now().format("%d/%m/%Y, %H:%M").to_string();
-        let link = format!("{}/{}", self.host.url, self.key);
+        let url = format!("{}/{}", self.host.url, self.key);
 
         let mut embed = CreateEmbed::new()
             .title("Stream Offline")
+            .url(url)
             .color(0xE74C3C)
             .description(format!("{} is offline.", self.key))
-            .field("Link", link, false)
             .field(EMPTY_STR, EMPTY_STR, false) // Add a blank line to separate the fields from the description
             .footer(CreateEmbedFooter::new(format!("Ended: {}", ended)));
 
