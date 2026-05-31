bc1ba6ec7a4438fd1bcb541149a9e072d3fe66a3
richardtatum <rtatum@pm.me>
2026-05-31 17:53:35 +0100

    Add guildId check

--- crates/adapters/discord/src/commands/add_host.rs
+++ crates/adapters/discord/src/commands/add_host.rs
@@ -14,5 +14,14 @@ use poise::{
 
 #[poise::command(slash_command)]
 pub async fn add_host<P: PersistenceProvider>(ctx: Context<'_, P>) -> Result<(), Error> {
+    let Some(guild_id) = ctx.guild_id() else {
+        let embed = error_embed("Subscribe Failed", "/subscribe can only be ran from a server channel currently.");
+        ctx.send(CreateReply::default().embed(embed)).await?;
+        return Ok(());
+    };
+
+    let user_id = ctx.author().id;
+    let channel_id = ctx.channel_id();
+
     Ok(())
 }
