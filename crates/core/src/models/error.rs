6f6ac40422ac58142c362cb1dda56a8c615bc5b9
Richard Tatum <rtatum@pm.me>
2026-06-02 18:20:18 +0100

    Add a method for extracting the message from an error

--- crates/core/src/models/error.rs
+++ crates/core/src/models/error.rs
@@ -7,6 +7,16 @@ pub enum CoreError {
     NotificationError(String),
 }
 
+impl CoreError {
+    pub fn message(&self) -> &str {
+        match self {
+            CoreError::StreamError(msg) => msg,
+            CoreError::PersistenceError(msg) => msg,
+            CoreError::NotificationError(msg) => msg,
+        }
+    }
+}
+
 // Implement display and std::error so that CoreError can be cast into the discord type error
 impl fmt::Display for CoreError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
