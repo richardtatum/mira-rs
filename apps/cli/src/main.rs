ad00a02a5d3a7008908cd8aa6934ebcc54d9ba05
richardtatum <rtatum@pm.me>
2026-07-12 21:12:04 +0100

    Add ts to CLI

--- apps/cli/src/main.rs
+++ apps/cli/src/main.rs
@@ -53,6 +53,8 @@ enum Commands {
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::fmt().with_env_filter(tracing_subscriber::EnvFilter::from_default_env()).init();
+
     let cli = Cli::parse();
 
     match cli.command {
